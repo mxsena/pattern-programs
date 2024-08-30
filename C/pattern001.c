95581216bdec94b614629b38f54ddb2032980819
Matheus Sena <mpwsena@icloud.com>
2024-08-30 11:38:25 -0300

    pattern003

--- C/pattern001.c
+++ C/pattern001.c
@@ -10,11 +10,13 @@
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    for (int i = 0; i < 5; i++)
+    int i, j;
+
+    for (i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (j = 0; j < 5; j++)
         {
             printf("* ");
         }
