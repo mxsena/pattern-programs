f89f3e27b3d109034ea4f88a9a442193d43619c6
Matheus Sena <mpwsena@icloud.com>
2024-09-02 14:41:58 -0300

    update

---
+++ C/pattern005.c
@@ -0,0 +1,25 @@
+/*
+
+======= PATTERN 005 =======
+
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+
+*/
+
+#include <stdio.h>
+
+    int main(void) {
+        int i, j;
+
+        for (i = 5; i >= 1; i--) {
+            for (j = 5; j >= 1; j--) {
+                printf("%d ", j);
+            }
+            printf("\n");
+        }
+        return 0;
+    }
