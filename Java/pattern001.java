b06ea8ac1dbc6d9f913206928d1262d894b3d755
Matheus Sena <mpwsena@icloud.com>
2024-09-03 14:14:01 -0300

    update

--- Java/pattern001.java
+++ Java/pattern001.java
@@ -12,7 +12,7 @@
 
 package Java;
 
-class pattern001 {
+public class pattern001 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
--- Java/pattern003.java
+++ Java/pattern003.java
@@ -1,22 +1,22 @@
 /*
 
-======= PATTERN 002 =======
+======= PATTERN 003 =======
 
-1 1 1 1 1
-2 2 2 2 2
-3 3 3 3 3
-4 4 4 4 4
-5 5 5 5 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
 
 */
 
 package Java;
 
-public class pattern002 {
+public class pattern003 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
-                System.out.print(i + " ");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
