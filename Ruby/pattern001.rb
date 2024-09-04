27bca32549a6c9bdd612e5e8c29e1e722a072f37
Matheus Sena <mpwsena@icloud.com>
2024-09-04 18:05:02 -0300

    update

---
+++ Ruby/pattern001.rb
@@ -0,0 +1,22 @@
+=begin
+―――――――― PATTERN 001
+――――――――――――――――――――
+.       * * * * *
+.       * * * * *
+.       * * * * *
+.       * * * * *
+.       * * * * *
+
+――――――――――――――――――――
+=end
+
+def create_pattern(rows, columns)
+    for row in 1..rows do
+      for column in 1..columns do
+        print "* "
+      end
+      puts
+    end
+  end
+  
+  create_pattern(5, 5)
