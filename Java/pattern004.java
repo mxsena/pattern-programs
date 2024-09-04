727ad63c9f84f698e30b9ef57eac53997f583f48
Matheus Sena <mpwsena@icloud.com>
2024-09-04 10:03:57 -0300

    Update pattern004.java

--- Java/pattern004.java
+++ Java/pattern004.java
@@ -1,5 +1,24 @@
+/*
+
+======= PATTERN 004 =======
+
+5 5 5 5 5
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+
+*/
+
 package Java;
 
 public class pattern004 {
-    
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <=5; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
 }
