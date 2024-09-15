422b418df6894ab2b86e487bec80e1a010382818
Matheus Sena <mpwsena@icloud.com>
2024-09-14 23:35:16 -0300

    update

---
+++ C++/pattern004.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int i, j;
+    for (i=5;i>=1;i--){
+        for(j=1;j<=5;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ C++/pattern005.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    int i,j;
+    for(i=5;i>=1;i--){
+        for(j=5;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
