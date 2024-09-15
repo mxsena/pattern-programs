ca2d5fa2fc56ba5ccab562c659a1e9b7ebf5b793
Matheus Sena <mpwsena@icloud.com>
2024-09-14 23:42:05 -0300

    Create pattern006.cpp

---
+++ C++/pattern006.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    int n=5,k=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<k++<<" ";
+        }
+    cout<<endl;
+    }
+    return 0;
+}
+// !fix
