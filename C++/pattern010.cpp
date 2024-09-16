9d25019cd2d4bd0a83a9d374a45417c1a150d668
Matheus Sena <mpwsena@icloud.com>
2024-09-15 23:43:41 -0300

    Create pattern010.cpp

---
+++ C++/pattern010.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    for(int i=1;i<=5;i++){
+        for(int j=1; j<=3; j++){
+            cout<<j<<" "<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
