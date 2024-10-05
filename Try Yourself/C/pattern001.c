8eef3e02fbb0530f8a237466abe490b61631ec57
Matheus Sena <mpwsena@icloud.com>
2024-10-05 13:59:44 -0300

    update

---
+++ Try Yourself/C/pattern001.c
@@ -0,0 +1,15 @@
+// Pattern Programs n.001 [c]
+// Author: https://github.com/mxsena
+
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+#include <stdio.h>
+
+int main(void) {
+    // Your solution:
+
+}
