77503a5058d23edcae228b4b686e2052f3458b2c
Matheus Sena <mpwsena@icloud.com>
2024-10-18 23:11:29 -0300

    update

--- Solutions/C/pattern012.c
+++ Solutions/C/pattern012.c
@@ -2,14 +2,17 @@
 // Github Repository: https://github.com/mxsena/pattern-programs
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int n = 5;
     int i, j;
     int x;
 
-    for(i = 1; i <= n; i++) {
+    for (i = 1; i <= n; i++)
+    {
         x = i;
-        for (j = 1; j <= n; j++) {
+        for (j = 1; j <= n; j++)
+        {
             printf("%2d ", x);
             x += n;
         }
