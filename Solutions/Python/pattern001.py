7b81ba6c6ebe203bf4bf76f39254e339bebf449e
Matheus Sena <mpwsena@icloud.com>
2024-10-18 23:15:11 -0300

    update

--- Solutions/Python/pattern001.py
+++ Solutions/Python/pattern001.py
@@ -1,6 +1,6 @@
 # Pattern Programs n.001 [Python]
 # Github Repository: https://github.com/mxsena/pattern-programs
-for x in range (1, 6):
-    for y in range (1, 6):
+for x in range(1, 6):
+    for y in range(1, 6):
         print("*", end=" ")
     print()
