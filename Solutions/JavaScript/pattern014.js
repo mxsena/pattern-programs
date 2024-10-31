9ff080d5bbd96983a016c83e52a7d1a444f33ef4
Matheus Sena <mpwsena@icloud.com>
2024-10-31 07:34:45 -0300

    Create pattern014.js

---
+++ Solutions/JavaScript/pattern014.js
@@ -0,0 +1,13 @@
+let pattern = " ";
+let n = 5;
+let x;
+
+for (let i = 1; i <= n; i++) {
+    x = n - i + 1;
+    for (let j = 1; j<= n; j++) {
+        pattern += x.toString().padStart(3, " ");
+        x = x + n;
+    }
+    pattern += "\n";
+}
+console.log(pattern)
